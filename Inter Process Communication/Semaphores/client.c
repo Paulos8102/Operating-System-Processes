8b52f10e68f5f50b6a0a111b2192414bdf0d409e
Paul Jabez T <88676086+Paulos8102@users.noreply.github.com>
2021-10-16 14:58:58 +0530

    Create client.c

---
+++ Inter Process Communication/Semaphores/client.c
@@ -0,0 +1,76 @@
+//client.c:
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/wait.h>
+#include<time.h>
+#include <sys/errno.h>
+#include <sys/types.h>
+ 
+#define SIZE 3 /* size of the shared buffer */
+#define SHMPERM 0666 /* shared memory permissions */
+ 
+int segid; /* ID for shared memory buffer */
+ 
+sem_t *empty;
+sem_t *full;
+sem_t *mutex;
+ 
+char *buff;
+int i= 0,N=5;
+ 
+int main()
+{
+    srand(time(0));
+ 
+    segid = shmget(100, SIZE, IPC_CREAT|0666);
+    buff = shmat(segid, (char *)0, 0);
+ 
+    int empty_id=shmget(101,sizeof(sem_t),IPC_CREAT|0666);
+    empty = shmat(empty_id,(char *)0,0);
+ 
+    int full_id=shmget(102,sizeof(sem_t),IPC_CREAT|0666);
+    full = shmat(full_id,(char *)0,0);
+ 
+    int mutex_id=shmget(103,sizeof(sem_t),IPC_CREAT|0666);
+    mutex = shmat(mutex_id,(char *)0,0);
+ 
+    while(1){
+        if(!N){
+            break;
+        }
+        printf("Client trying to acquire semaphore full\n");
+        sem_wait(full);
+        printf("Semaphore full acquired by client\n");
+        printf("Client trying to acquire semaphore mutex\n");
+        sem_wait(mutex);
+        printf("Semaphore mutex acquired by client\n");
+        printf("Reading %c\n",buff[i]);
+        i++;
+        N--;
+        sem_post(empty);  
+        printf("Client released semaphore empty\n");
+        sem_post(mutex);
+        printf("Client released semaphore mutex\n\n");
+        sleep(1);
+    }
+ 
+    shmdt(buff);
+    shmdt(empty);
+    shmdt(full);
+    shmdt(mutex);
+    shmctl(segid, IPC_RMID, NULL);
+    semctl( empty_id, 0, IPC_RMID, NULL);
+    semctl( full_id, 0, IPC_RMID, NULL);
+    semctl( mutex_id, 0, IPC_RMID, NULL);
+    sem_destroy(empty);
+    sem_destroy(full);
+    sem_destroy(mutex);
+}
