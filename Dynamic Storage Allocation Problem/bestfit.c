bf2c7f212bdc8673371bff71048403f0c7c66ef0
Paul Jabez T <88676086+Paulos8102@users.noreply.github.com>
2021-10-18 11:04:20 +0530

    Update bestfit.c

--- Dynamic Storage Allocation Problem/bestfit.c
+++ Dynamic Storage Allocation Problem/bestfit.c
@@ -21,3 +21,26 @@ void main()
             printf("Process no.%d:",i);
             scanf("%d",&p[i]);
         }
+    
+    for(i=1;i<=np;i++)
+    {
+        for(j=1;j<=nb;j++)
+        {
+            if(barray[j]!=1)
+            {
+                temp=b[j]-p[i]; if(temp>=0) if(lowest>temp)
+            {
+            parray[i]=j; lowest=temp;
+            }
+            }
+        }
+        fragment[i]=lowest; barray[parray[i]]=1; 
+        lowest=10000;
+    }
+    
+
+    printf("\nProcess_no\tProcess_size\tBlock_no\tBlock_size\tFragment"); 
+    for(i=1;i<=np && parray[i]!=0;i++) 
+        printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,p[i],parray[i],b[parray[i]],fragment[i]);
+    printf("\n");
+}
